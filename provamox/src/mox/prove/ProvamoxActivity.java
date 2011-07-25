fe91336df108e9c8f4acf716bdc3a0f7d0ea5264
Enrico Bandiera <e.bandiera@gmail.com>
2011-07-25 16:42:07 +0200

    primo commit

---
+++ provamox/src/mox/prove/ProvamoxActivity.java
@@ -0,0 +1,17 @@
+package mox.prove;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ProvamoxActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        TextView t1 = (TextView) findViewById(R.id.tv1);
+        t1.setText("provaaaaaaaaaa");
+    }
+}
