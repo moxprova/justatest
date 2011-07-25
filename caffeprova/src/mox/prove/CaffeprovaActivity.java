8779fd6cf00a0b35760ab807da13bb79d98ab27d
Enrico Bandiera <e.bandiera@gmail.com>
2011-07-25 17:16:08 +0200

    xxxxxxx

--- caffeprova/src/mox/prove/CaffeprovaActivity.java
+++ caffeprova/src/mox/prove/CaffeprovaActivity.java
@@ -9,5 +9,7 @@ public class CaffeprovaActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //abcdefghijklmn
     }
 }
